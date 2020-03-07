0732615a5316b024da4e22a81344c04919c24a17
Manuel Valle <FcoManueel@gmail.com>
2020-03-07 12:24:38 -0700

    Add bare main.go and project roadmap

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO Add CLI flags, parse and validation
+	// TODO Start url download and return stream
+	// TODO Implement throttling for download
+	// TODO Implement stream hasher
+	// TODO Connect download stream stream to hasher
+	// TODO Expose hash in hexadecimal
+	// TODO Write file
+	// TODO Update README.md
+
+	fmt.Println("TODO: do todos")
+}
